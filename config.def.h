2d8bfc928d4bfbe388465b7dd9950294cfa9dc6b
Shashwat Sarkar <shashwatsarkar22@gmail.com>
2022-10-05 19:41:58 +0530

    minor changes

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */ 
 /* appearance */ 
-static const unsigned int borderpx  = 2;        /* border pixel of windows */ 
+static const unsigned int borderpx  = 1;        /* border pixel of windows */ 
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" }; 
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11"; 
 static const unsigned int snap      = 32;       /* snap pixel */ 
-static const unsigned int gappih    = 5;        /* horiz inner gap between windows */ 
-static const unsigned int gappiv    = 5;        /* vert inner gap between windows */ 
-static const unsigned int gappoh    = 5;        /* horiz outer gap between windows and screen edge */ 
-static const unsigned int gappov    = 5;        /* vert outer gap between windows and screen edge */ 
+static const unsigned int gappih    = 7;        /* horiz inner gap between windows */ 
+static const unsigned int gappiv    = 7;        /* vert inner gap between windows */ 
+static const unsigned int gappoh    = 7;        /* horiz outer gap between windows and screen edge */ 
+static const unsigned int gappov    = 7;        /* vert outer gap between windows and screen edge */ 
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */ 
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */ 
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -18,17 +18,18 @@ static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "kitty", "-T", scratchpadname, NULL };
+static const char col_gray0[]       = "#0a0a0a";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#9e9d9d";
 static const char col_gray5[]       = "#4e90e6";
-static const char col_cyan[]        = "#e61207";
-static const char col_redl[]        = "#ff7878";
+static const char col_cyan[]        = "#7dd5e3";
+static const char col_cyand[]        = "#d7a6ff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray4, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_redl, col_gray1,  col_cyan  },
+	[SchemeSel]  = { col_gray0, col_cyand,  col_cyand  },
 };
 
 /* tagging */
